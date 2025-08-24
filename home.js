c73e6a758d64aab0b3ef87f30cc673360f38563f
Mahidkarim <dataof098@gmail.com>
2025-08-25 00:29:56 +0600

    added add money section succefully

--- home.js
+++ home.js
@@ -1,24 +1,40 @@
-document.getElementById('add-btn').addEventListener('click', function(e){
-     e.preventDefault()
+
+
+const pin = 1234
+
+document.getElementById('add-btn').addEventListener('click', function (e) {
+    e.preventDefault()
 
 
 
-     const selectBankValue = document.getElementById("select-bank").value
+    const selectBankValue = document.getElementById("select-bank").value
 
-     const accountNumberValue = document.getElementById("account-Number").value
-
-     const addAmountValue = parseInt(document.getElementById("add-amount").value)
-
-     const pinNumberValue = document.getElementById("pin-number").value
+    const accountNumberValue = document.getElementById("account-Number").value
 
 
-        const totalBalanceValue = parseInt(document.getElementById('balance').innerText)
+    const addAmountValue = parseInt(document.getElementById("add-amount").value)
 
-        
-         const totalAmount = addAmountValue + totalBalanceValue;
+    const pinNumberValue = parseInt(document.getElementById("pin-number").value)
 
-        document.getElementById('balance').innerText = totalAmount
+    if( accountNumberValue.length < 11 ){
+        alert('Please Provide a Valid Account Number')
+        return;
+    }
+
+    else if(pinNumberValue !== pin){
+        alert('Please provide a valid pin')
+        return;
+
+    }
 
 
-    
+    const totalBalanceValue = parseInt(document.getElementById('balance').innerText)
+
+
+    const totalAmount = addAmountValue + totalBalanceValue;
+
+    document.getElementById('balance').innerText = totalAmount
+
+
+
 })
